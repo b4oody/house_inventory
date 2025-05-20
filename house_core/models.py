1e80403cfcfeb662dd60da90af5d7aff4df31fcb
VR <vladrymarchuk@gmail.com>
2025-05-20 23:57:54 +0300

    update: add verbose_name for every field in Apartment model

--- house_core/models.py
+++ house_core/models.py
@@ -18,19 +18,28 @@ class User(AbstractUser):
 
 
 class Apartment(models.Model):
-    apartment_name = models.CharField(max_length=100, unique=True)
+    apartment_name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name="Apartment Name",
+    )
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name="apartments"
     )
-    address = models.CharField(max_length=100)
-    apartment_description = models.TextField(blank=True, null=True)
+    address = models.CharField(max_length=100, verbose_name="Address",)
+    apartment_description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Description",
+    )
     purchase_price = models.DecimalField(
         max_digits=10,
         decimal_places=2,
         blank=True,
-        null=True
+        null=True,
+        verbose_name="Purchase Price ₴",
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
