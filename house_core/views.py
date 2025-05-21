194e01892640c511af2f64ee80522313226cfb16
VR <vladrymarchuk@gmail.com>
2025-05-21 14:41:56 +0300

    feat: add views for index, items and apartments

---
+++ house_core/views.py
@@ -0,0 +1,54 @@
+from django.core.paginator import Paginator
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+from house_core.models import Item, Apartment
+
+
+def pagination(request, model_of_list):
+    """
+    :param request:
+    :param model_of_list:
+    :return:
+    """
+    paginator = Paginator(model_of_list, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return page_obj
+
+
+def index_page_view(request: HttpRequest) -> HttpResponse:
+    return render(request, "index/index.html")
+
+
+def items_page_view(request: HttpRequest) -> HttpResponse:
+    context = {
+        "items": Item.objects.all(),
+    }
+    return render(
+        request,
+        "items/items.html",
+        context=context
+    )
+
+
+def apartments_page_view(request: HttpRequest) -> HttpResponse:
+    exclude = {"id", "user", "created_at"}
+    apartments = Apartment.objects.all()
+    page_obj = pagination(request, apartments)
+
+    fields = [
+        field for field in Apartment._meta.concrete_fields
+        if field.name not in exclude
+    ]
+
+    context = {
+        "apartments_fields": fields,
+        "page_obj": page_obj,
+        "apartments": page_obj
+    }
+    return render(
+        request,
+        "apartments/apartments.html",
+        context=context
+    )
