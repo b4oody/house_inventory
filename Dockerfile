71cfb0198b49b8e280b5960eb379376600afcfa1
VR <vladrymarchuk@gmail.com>
2025-06-28 20:00:44 +0300

    feat: add nginx

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,37 @@
 FROM python:3.10.8
 LABEL maintainer="b4oody"
 
-ENV PYTHONNBUFFERED=1
+ENV PYTHONUNBUFFERED=1
 ENV PYTHONDONTWRITEBYTECODE=1
 
-WORKDIR app/
+WORKDIR /app/
+
 
 RUN apt-get update && apt-get install -y netcat-traditional
+
 
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
+
 COPY . .
 
-RUN mkdir -p /files/media
+COPY ./entrypoint.sh /app/entrypoint.sh
+RUN chmod +x /app/entrypoint.sh
 
 RUN adduser \
     --disabled-password \
     --no-create-home \
     my_user
 
-RUN chown -R my_user /files/media
-RUN chmod -R 755 /files/media
+RUN mkdir -p /app/staticfiles /app/media
+RUN chown -R my_user:my_user /app
 
-
-COPY ./entrypoint.sh /app/entrypoint.sh
-RUN chmod +x /app/entrypoint.sh
 
 USER my_user
 
+
 ENTRYPOINT ["/app/entrypoint.sh"]
 
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+
+CMD ["gunicorn", "config.wsgi:application", "--bind", "0.0.0.0:8000"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
 
-# Чекаємо, поки PostgreSQL буде готовий
-# Використовуємо змінні з .env файлу
+# Зупинити виконання скрипта, якщо будь-яка команда зазнає невдачі
+set -e
+
+# Чекаємо, поки база даних буде готова
 echo "Waiting for postgres..."
 while ! nc -z $POSTGRES_HOST $POSTGRES_DB_PORT; do
   sleep 0.1
 done
 echo "PostgreSQL started"
 
-# Застосовуємо міграції
+# Збираємо всі статичні файли в STATIC_ROOT
+echo "Collecting static files..."
+python manage.py collectstatic --no-input --clear
+
+# Застосовуємо міграції бази даних
+echo "Applying database migrations..."
 python manage.py migrate
 
-# Завантажуємо фікстури
-python manage.py loaddata items_fixture.json
+# Завантажуємо початкові дані (фікстури)
+echo "Loading fixtures..."
+python manage.py loaddata items_fixture.json # або ваші фікстури
 
-# Запускаємо основну команду
+# Передаємо управління основній команді (gunicorn),
+# яка вказана в docker-compose.yml або Dockerfile
+echo "Starting gunicorn..."
 exec "$@"
